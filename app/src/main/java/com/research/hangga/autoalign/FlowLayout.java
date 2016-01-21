a0a90b8921564031c9f9271b481b82774b482bb4
sayekti <sayekti@sebangsa.com>
2016-01-21 19:41:57 +0700

    sample

---
+++ app/src/main/java/com/research/hangga/autoalign/FlowLayout.java
@@ -0,0 +1,99 @@
+package com.research.hangga.autoalign;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hangga on 21/01/16.
+ */
+
+public class FlowLayout extends ViewGroup {
+
+    private int paddingHorizontal;
+    private int paddingVertical;
+
+    public FlowLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+        init();
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        paddingHorizontal = this.getPaddingLeft();
+        paddingVertical = this.getPaddingTop();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int mHeight = 0;
+
+        int childLeft = getPaddingLeft();
+        int childTop = getPaddingTop();
+        int myWidth = resolveSize(getWidth(), widthMeasureSpec);
+        int wantedHeight = 0;
+
+        for (int i = 0; i < getChildCount(); i++) {
+
+            final View thisChild = getChildAt(i);
+            if (thisChild.getVisibility() == View.GONE) {
+                continue;
+            }
+
+            thisChild.measure(getChildMeasureSpec(widthMeasureSpec, 0, thisChild.getLayoutParams().width),
+                    getChildMeasureSpec(heightMeasureSpec, 0, thisChild.getLayoutParams().height));
+
+            int childWidth = thisChild.getMeasuredWidth();
+            int childHeight = thisChild.getMeasuredHeight();
+
+            mHeight = Math.max(childHeight, mHeight);
+
+            if (childWidth + childLeft + getPaddingRight() > myWidth) {
+                childLeft = getPaddingLeft();
+                childTop += paddingVertical + mHeight;
+                mHeight = childHeight;
+            }
+
+            childLeft += childWidth + paddingHorizontal;
+        }
+        wantedHeight += childTop + mHeight + getPaddingBottom();
+        setMeasuredDimension(myWidth, resolveSize(wantedHeight, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int childLeft = getPaddingLeft();
+        int childTop = getPaddingTop();
+        int lineHeight = 0;
+
+        int myWidth = right - left;
+
+        for (int i = 0; i < getChildCount(); i++) {
+            final View child = getChildAt(i);
+            if (child.getVisibility() == View.GONE) {
+                continue;
+            }
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
+            lineHeight = Math.max(childHeight, lineHeight);
+            if (childWidth + childLeft + getPaddingRight() > myWidth) {
+                childLeft = getPaddingLeft();
+                childTop += paddingVertical + lineHeight;
+                lineHeight = childHeight;
+            }
+            child.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+            childLeft += childWidth + paddingHorizontal;
+        }
+    }
+}
+
